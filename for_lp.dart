1948b07460276c69f5bca299d7cf231a8888dc06
Popeaura <popeahura@gmail.com>
2024-11-07 14:56:02 +0300

    I added and modified my file

---
+++ for_lp.dart
@@ -0,0 +1,9 @@
+
+/* void main()  
+{  
+    var list1 = [10,20,30,40,50];  
+    for(var i in list1)           //for..in loop to print list element  
+    {  
+        print(i);       //to print the number  
+    }  
+}  */
