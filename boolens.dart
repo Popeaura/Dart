885f54f7484db97590ad762529a4a6aa9c6d3ac1
Popeaura <popeahura@gmail.com>
2024-11-07 14:47:07 +0300

    I added and modified my file

--- boolens.dart
+++ boolens.dart
@@ -1,19 +1,18 @@
-void main(){
-  //Boolean code: True or False logic
+void main() {
+  // Boolean code: True or False logic
   bool isCodingFun = true;
   bool lovesDebugging = false;
 
-//making decisions using booleans 
-if(isCodingFun){
-  print('Coding is fun keep going !');
-}else{
-  print ('Try a new language or project for more fun !');
-}
+  // Making decisions using booleans
+  if (isCodingFun) {
+    print('Coding is fun, keep going!');
+  } else {
+    print('Try a new language or project for more fun!');
+  }
 
-if(lovesDebugging){
-  print('Debugging is like solving a puzzle !');
-}else{
-  print('Focus on writing bug free codes !');
-}
-
+  if (lovesDebugging) {
+    print('Debugging is like solving a puzzle!');
+  } else {
+    print('Focus on writing bug-free code!');
+  }
 }
