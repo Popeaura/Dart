8d66e0caefad806764c7f376ec751a36a24a7e94
Popeaura <popeahura@gmail.com>
2024-11-07 15:16:00 +0300

    I added and modified my file

--- for_lp.dart
+++ for_lp.dart
@@ -7,3 +7,25 @@
         print(i);       //to print the number  
     }  
 }  */
+
+/*
+void main() {
+  var list1 = [10, 20, 30, 40, 50];
+  int i = 0;            // Initialize index
+
+  while (i < list1.length) {  // Loop until i is less than the length of the list
+    print(list1[i]);          // Print the current element at index i
+    i++;                      // Increment the index
+  }
+}
+*/
+ void main()  
+{  
+ var a = 1;  
+ var maxnum = 10;  
+do  
+    {                
+       print("The value is: ${a}");  
+       a = a+1;                                    
+       }while(a<maxnum);  
+}  
