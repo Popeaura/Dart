89700af65c8c0425c8d1574d3c0cffd65c520637
Popeaura <popeahura@gmail.com>
2024-11-18 00:50:06 +0300

    I modified my projects folder

--- maps.dart
+++
@@ -1,9 +0,0 @@
-void main() {
-  //creating a map with string keys and int values
-  Map<String, int> ages = {
-    'Alice':30,
-    'Bob': 25,
-    'Charlie':35,
-  };
-  print("Ages of students : $ages");
-}
